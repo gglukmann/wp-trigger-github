615b5346fad2b81c255dec69e2a603e62c430149
Gert Glükmann <gglukmann@gmail.com>
2019-10-11 11:50:50 +0300

    Everything to git

---
+++ README.md
@@ -0,0 +1,1 @@
+# wp-trigger-github
---
+++ wp-trigger-github.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package WPTriggerGithub
+ */
+/*
+Plugin Name: WP Trigger Github
+Plugin URI: https://github.com/gglukmann/wp-trigger-github
+Description: Publish or save action trigger github repository_dispatch action
+Version: 1.0.0
+Author: Gert Glükmann
+Author URI: https://github.com/gglukmann
+License: GPLv2 or later
+Text-Domain: wp-trigger-github
+ */
+
+
+?>
